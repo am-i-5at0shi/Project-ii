838efe8b411264515921fbc28dd8ef904774a512
Krishna Prabhakar <krishna_csb18@agnee.tezu.ernet.in>
2021-12-25 22:53:20 +0530

    Pulp API

---
+++ code_linear_prograamming.py
@@ -0,0 +1,21 @@
+from pulp import LpMaximize,LpProblem,LpStatus,lpSum,LpVariable
+model = LpProblem(name="",sense=LpMaximize)
+x = LpVariable(name="x", lowBound=0)
+y = LpVariable(name="y", lowBound=0)
+model += (2 * x +  y <= 20, "red_constraint")
+model += ((-4) * x + 5 * y <= 10, "blue_constraint")
+model += (-x + 2 * y >= -2, "yellow_constraint")
+#model += (-x + 5 * y == 15, "green_constraint")
+obj_func = x + 2 * y
+model += obj_func
+status = model.solve()
+print(">>>",model)
+print("------------------------------------------------")
+coeff = [1, 2]
+i=0;
+z=0;
+for var in model.variables():
+	print("X",i+1,"= ",var.value(),sep="")
+	z+=coeff[i]*var.value()
+	i+=1
+print("max(z)=",z)
